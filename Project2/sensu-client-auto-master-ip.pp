afdc7ca3684b26629be33386b33bd7d30be38086
Getinet <gtntayele@gmail.com>
2018-02-10 02:52:49 +0100

    Create sensu-client-auto-master-ip.pp

---
+++ Project2/sensu-client-auto-master-ip.pp
@@ -0,0 +1,96 @@
+# VARIABLES
+########################################################
+
+$RABBITMQ_PASSWD = "password"          #"398hhgaihdliauhe893"
+
+$ADMIN_PASSWD = "sensuadminpass"
+
+$SENSU_SERVER_IP = "10.0.16.16"
+
+$SENSU_VERSION = "0.23"
+
+########################################################
+
+if $SENSU_SERVER_IP==undef {
+fail("You need to set the IP address of your sensu server in the SENSU_SERVER_IP variable")
+}
+
+
+$SENSU_CONFIG = "
+{
+      \"rabbitmq\": {
+        \"ssl\": {
+          \"private_key_file\": \"/etc/sensu/ssl/client_key.pem\",
+          \"cert_chain_file\": \"/etc/sensu/ssl/client_cert.pem\"
+        },
+        \"port\": 5671,
+        \"host\": \"$SENSU_SERVER_IP\",
+        \"user\": \"sensu\",
+        \"password\": \"$RABBITMQ_PASSWD\",
+        \"vhost\": \"/sensu\"
+      }
+     }
+"
+
+
+$SENSU_CLIENT_CONFIG = "
+{
+\"client\":{
+\"name\":\"$hostname\",
+\"address\":\"$ipaddress\",
+\"subscriptions\":[\"test\"]
+}
+}
+"
+
+# REDIS
+
+package { "redis-server" :
+ ensure => present,
+}
+
+service { "redis-server" :
+  ensure => running,
+  require => Package['redis-server'],
+}
+
+
+# SENSU
+     exec { "add-sensu-repo-key" :
+             path => "/usr/bin/:/usr/sbin/:/usr/local/bin:/bin/:/sbin",
+             command => "curl http://repositories.sensuapp.org/apt/pubkey.gpg | apt-key add - ",
+             unless => "ls /etc/apt/sources.list.d/sensu.list",
+	     require => Package['redis-server'],
+     }
+
+     exec { "add-sensu-repo" :
+             path => "/usr/bin/:/usr/sbin/:/usr/local/bin:/bin/:/sbin",
+             command => "echo 'deb     https://sensu.global.ssl.fastly.net/apt trusty main' >> /etc/apt/sources.list.d/sensu.list ; apt-get update",
+             require => Exec["add-sensu-repo-key"],
+             unless => "ls /etc/apt/sources.list.d/sensu.list"
+     }
+
+    package { "sensu" :
+      ensure => present,
+      require => Exec['add-sensu-repo'],
+      }
+    
+    file { "/etc/sensu/ssl" :
+      ensure => directory,
+      mode => 644,
+      require => Package['sensu'],
+     }
+     
+    
+   file { "/etc/sensu/conf.d/client.json":
+     ensure => present,
+     content => $SENSU_CLIENT_CONFIG,
+     require => File['/etc/sensu/ssl'],
+   }
+   
+    file { "/etc/sensu/config.json" :
+       ensure => present,
+       content => $SENSU_CONFIG,
+       require => Package['sensu'],
+       notify => File['/etc/sensu/ssl'],
+    }
